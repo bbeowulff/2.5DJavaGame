53f65ab6f45e2daad3a90fc12de6f6022cf35bc5
Alex <Alex@DESKTOP-V7EGPG4>
2024-04-03 12:19:35 +0300

    Update NPC

---
+++ src/entity/NPC_OldMan.java
@@ -0,0 +1,76 @@
+package entity;
+
+import java.awt.Rectangle;
+import java.util.Random;
+
+import main.GamePanel;
+
+
+
+public class NPC_OldMan extends Entity {
+	
+	public NPC_OldMan(GamePanel gp) {
+		super(gp);
+		
+		direction = "down";
+		speed = 1;
+		
+		// solid area npc
+		solidArea = new Rectangle();
+		solidArea.x=12;
+		solidArea.y=24;
+		solidAreaDefaultX = solidArea.x;
+		solidAreaDefaultY = solidArea.y;
+		solidArea.width = 24;
+		solidArea.height = 24;
+		
+		getImage();
+	}
+	public void getImage() {
+		
+		up1_idle = setup("/npc/back_idle");
+		up2_idle = setup("/npc/back_idle");
+		up1 = setup("/npc/back_run1");
+		up2 = setup("/npc/back_run2");
+		down1_idle = setup("/npc/down_idle");
+		down2_idle = setup("/npc/down_idle");
+		down1 = setup("/npc/down1");
+		down2 = setup("/npc/down2");
+		left1_idle = setup("/npc/left_idle");
+		left2_idle = setup("/npc/left_idle");
+		left1 = setup("/npc/left1");
+		left2 = setup("/npc/left2");
+		right1_idle = setup("/npc/right_idle");
+		right2_idle = setup("/npc/right_idle");
+		right1 = setup("/npc/right1");
+		right2 = setup("/npc/right2");
+	}
+	
+	public void setAction() {
+		
+		actionLockCounter ++;
+		
+		if(actionLockCounter == 120) {
+			Random random = new Random();
+			int i = random.nextInt(100) + 1;  //1-100
+			
+			if(i <= 25) {
+				direction = "up";
+			}
+			if(i > 25 && i <= 50) {
+				direction = "down";
+			}
+			if(i > 50 && i <= 75) {
+				direction = "left";
+			}
+			if(i > 75 && i <= 100) {
+				direction = "right";		
+			}
+			
+			actionLockCounter = 0;
+		
+		
+		}
+	}
+
+}
